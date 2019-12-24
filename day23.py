1cbbbd2384e3b600bcc48b9c3c7531bf1209943e
agubelu <agustinbd3@gmail.com>
2019-12-24 13:26:38 +0100

    Small improvement in Day 23
    Make the process finish gracefully after the solution has been found,
    and switch from Process to Thread to avoid clogging up the CPU

--- day23.py
+++ day23.py
@@ -1,10 +1,9 @@
-# It seems to take a lot less time running on Linux (Ubuntu on Windows in my case) compared to pure Windows
-# Also it never stops, you have to CTRL+C it
-
 from etc import intcode
-from multiprocessing import Process, Manager
+from multiprocessing import Manager, Event
+from multiprocessing.managers import RemoteError
+from threading import Thread
 
-def run_computer(i, queues):
+def run_computer(i, queues, finished):
     comp = intcode.from_file("input/day23.txt")
 
     def get_packet(i):
@@ -16,28 +15,29 @@ def run_computer(i, queues):
     if i != 50:
         # Regular computer
         while True:
-            addr, x, y = comp.get_next_output(), comp.get_next_output(), comp.get_next_output()
-            if addr == 255: addr = 50
-            queues[addr].put([x, y])
+            try:
+                addr, x, y = comp.get_next_output(), comp.get_next_output(), comp.get_next_output()
+                if addr == 255: addr = 50
+                queues[addr].put([x, y], False)
+            except RemoteError: pass  # Main process is closing
     else:
         # NAT
         last_msg = None, None
-        printed_part2 = False  # It likes to print part 2 many times
         cur_msg = queues[i].get()
         
         if not last_msg[0]:  # Prints parts 1
             print(cur_msg[1])
 
-        while True:
+        while not finished.is_set():
             while not all(q.empty() for q in queues[:50]): 
                 pass
 
             while not queues[i].empty(): 
                 cur_msg = queues[i].get()
 
-            if last_msg[1] == cur_msg[1] and not printed_part2:
+            if last_msg[1] == cur_msg[1]:
                 print(cur_msg[1])  # Prints part 2
-                printed_part2 = True
+                finished.set()
 
             last_msg = cur_msg
             queues[0].put([cur_msg[0], cur_msg[1]])
@@ -45,6 +45,7 @@ def run_computer(i, queues):
 if __name__ == "__main__":  # Required so that Windows doesn't complain
     manager = Manager()
     list_of_qs = [manager.Queue() for _ in range(51)]
-    ps = [Process(target=run_computer, args=(i, list_of_qs)) for i in range(51)]
+    finished = Event()
+    ps = [Thread(target=run_computer, args=(i, list_of_qs, finished), daemon=True) for i in range(51)]
     for p in ps: p.start()
-    for p in ps: p.join()
+    finished.wait()
